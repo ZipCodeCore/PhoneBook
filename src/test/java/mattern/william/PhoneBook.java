dd3ac6a0042a682321f07d1fc3a2bae456b48c17
wmattern0 <wm.m.mattern@gmail.com>
2017-02-07 17:32:25 -0500

    4 methods built and tests passing

--- src/test/java/mattern/william/PhoneBook.java
+++ src/test/java/mattern/william/PhoneBook.java
@@ -9,19 +9,21 @@ public class PhoneBook {
     TreeMap<String,String> phoneBook = new TreeMap<String, String>();
 
     public void addEntry(String name, String phoneNumber){
+        phoneBook.put(name, phoneNumber);
 
     }
 
     public void deleteEntry(String name){
+        phoneBook.remove(name);
 
     }
 
     public String lookup(String name){
-        return null;
+        return phoneBook.get(name);
     }
 
     public String getAllNames(){
-        return null;
+        return phoneBook.entrySet().toString();
     }
 
 }
--- src/test/java/mattern/william/PhoneBookTest.java
+++ src/test/java/mattern/william/PhoneBookTest.java
@@ -9,29 +9,43 @@ import static org.junit.Assert.*;
  * Created by williammattern on 2/7/17.
  */
 public class PhoneBookTest {
+    PhoneBook phoneBook;
 
     @Before
     public void setUp(){
+        phoneBook = new PhoneBook();
+        phoneBook.addEntry("Hank", "123-456-7890");
 
     }
     @Test
-    public void addEntry() {
+    public void lookup() {
+        String expected = "123-456-7890";
+        String actual = phoneBook.lookup("Hank");
+        assertEquals(expected,actual);
+    }
 
+    @Test
+    public void addEntry() {
+        phoneBook.addEntry("Suzy", "444-444-4444");
+        String expected = "444-444-4444";
+        String actual = phoneBook.lookup("Suzy");
+        assertEquals(expected,actual);
     }
 
     @Test
     public void deleteEntry() {
-
-    }
-
-    @Test
-    public void lookup() {
-
+        phoneBook.deleteEntry("Hank");
+        String expected = null;
+        String actual = phoneBook.lookup("Hank");
+        assertEquals(expected,actual);
     }
 
     @Test
     public void getAllNames()  {
-
+        phoneBook.addEntry("Suzy", "444-444-4444");
+        String actual = phoneBook.getAllNames();
+        String expected = "[Hank=123-456-7890, Suzy=444-444-4444]";
+        assertEquals(expected,actual);
     }
 
 }
