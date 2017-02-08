4dc8117f99b70d0b1abf03c9e3197fad3008f88b
Randall Crame <randallcrame@zipcoders-MacBook-Pro.local>
2017-02-07 20:57:32 -0500

    Logic complete to part 3

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,3 +1,7 @@
+import com.sun.javafx.binding.StringFormatter;
+
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.TreeMap;
 
 /**
@@ -7,36 +11,108 @@ public class PhoneBook {
 
 
 
-    private TreeMap<String, String> addressBook = new TreeMap<>();
+    private TreeMap<String, ArrayList<String>> addressBook = new TreeMap<>();
 
-    public TreeMap<String, String> getAddressBook() {
+    public TreeMap<String, ArrayList<String>> getAddressBook() {
         return addressBook;
     }
 
-    public void setAddressBook(TreeMap<String, String> addressBook) {
-        this.addressBook = addressBook;
-    }
-
     public String lookUp(String K){
-        return null;
+        if (addressBook.isEmpty())
+            return "List Empty";
+        else if (K.length() > 32)
+            return "String > 32 characters";
+        else if (addressBook.containsKey(K)){
+            String list ="";
+            for (String value : addressBook.get(K))
+                list += value +"\n";
+            return list;
+        }
+        return "Name not listed";
     }
 
-    public void add(String K, String V){
+    public String add(String K, String V){
+        if (!addressBook.containsKey(K)){
+            ArrayList<String> list = new ArrayList<>();
+            addressBook.put(K, list);
+        }
 
+        if (K.length() < 32) {
+            addressBook.get(K).add(V);
+        } else return "String > 32 characters";
+        return K + " added";
     }
-    public void remove(String K){
-
+    public String remove(String K, String V){
+        if (addressBook.isEmpty())
+            return "List Empty";
+        else if (K.length() > 32)
+            return "String > 32 characters";
+        else if (addressBook.containsKey(K)) {
+            for (Map.Entry<String, ArrayList<String>> entry : addressBook.entrySet()) {
+                String key = entry.getKey();
+                for (String value : addressBook.get(key)) {
+                    if (V.equals(value)) {
+                        addressBook.get(key).remove(value);
+                        return "Removed Entry";
+                    }
+                }
+            }
+        }
+            return "Name not listed";
+    }
+    public String removeRecord(String K){
+        if (addressBook.isEmpty())
+            return "List Empty";
+        else if (K.length() > 32)
+            return "String > 32 characters";
+        else if (addressBook.containsKey(K)) {
+            addressBook.remove(K);
+            return K + "removed";
+        } else
+            return "Name not listed";
     }
 
     public String printNameList(){
-        return null;
+        if (addressBook.isEmpty())
+            return "List is empty";
+        else {
+            String list = "";
+            for (Map.Entry<String, ArrayList<String>> entry : addressBook.entrySet()) {
+                String key = entry.getKey();
+                list += key + "\n";
+            }
+        return list;
+        }
     }
 
     public String printEntryList(){
-        return null;
-    }
+        if (addressBook.isEmpty())
+            return "List is empty";
+        else {
+            String list = "";
+            for (Map.Entry<String, ArrayList<String>> entry : addressBook.entrySet()) {
+                String key = entry.getKey();
+                String value = lookUp(entry.getKey());
+                list += String.format("%s :\n %s\n", key, value);
+            }
+            return list;
+        }    }
 
     public String reverseLookUp(String V){
-        return null;
+        if (addressBook.isEmpty())
+            return "List is empty";
+        else if (V.length() > 32)
+            return "String > 32 characters";
+        else {
+            for (Map.Entry<String, ArrayList<String>> entry : addressBook.entrySet()) {
+                String key = entry.getKey();
+                for (String value : addressBook.get(key)) {
+                    if (V.equals(value))
+                        return key;
+                }
+            }
+
+        }
+            return "Name not listed";
     }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -13,7 +13,7 @@ public class PhoneBookTest {
             testName2 = "testName2",
             number = "555-555-5555",
             number2 = "555-555-5556",
-            tooLong = "jdiggjdneebsh3455hdbsssdghensjeeeqwhd";
+            tooLong = "jdiggjdneebsh3455hdbsssdghensjshkfdkfbkjafkbjabkjabjkasbjkeeeqwhd";
 
     @Before
     public void setUp(){
@@ -23,7 +23,7 @@ public class PhoneBookTest {
 
     @Test
     public void lookUpTest(){
-        String expected = number;
+        String expected = number+"\n";
         String actual = testBook.lookUp(testName);
         Assert.assertEquals("Expected number '555-555-5555' to return", expected, actual);
     }
@@ -37,7 +37,7 @@ public class PhoneBookTest {
 
     @Test
     public void lookUpTestEmpty(){
-        testBook.remove(testName);
+        testBook.removeRecord(testName);
         String expected = "List Empty";
         String actual = testBook.lookUp(testName);
         Assert.assertEquals("Expected number 'List Empty' to return", expected, actual);
@@ -57,6 +57,12 @@ public class PhoneBookTest {
     }
 
     @Test
+    public void removeTest(){
+        String expected = "Removed Entry";
+        String actual = testBook.remove(testName, number);
+        Assert.assertEquals("Expect 'Removed Entry' returned", expected, actual);
+    }
+    @Test
     public void addTest(){
         testBook.add(testName2, number2);
         int expected = 2;
@@ -73,8 +79,23 @@ public class PhoneBookTest {
     }
 
     @Test
-    public void removeTest(){
-        testBook.remove(testName);
+    public void addTestLongString(){
+        String expected = "String > 32 characters";
+        String actual = testBook.add(tooLong, number);
+        Assert.assertEquals("Expected number 'String > 32' to return", expected, actual);
+    }
+
+    @Test
+    public void addTestReplace(){
+        testBook.add(testName, number2);
+        String expected = number + "\n" + number2 + "\n";
+        String actual = testBook.lookUp(testName);
+        Assert.assertEquals("Expect address book to have replaced and return number2", expected, actual);
+    }
+
+    @Test
+    public void removeRecordTest(){
+        testBook.removeRecord(testName);
         int expected = 0;
         int actual = testBook.getAddressBook().size();
         Assert.assertEquals("Expects size to be 0", expected, actual);
@@ -82,8 +103,8 @@ public class PhoneBookTest {
 
     @Test
     public void removeTestEmpty(){
-        testBook.remove(testName);
-        testBook.remove(testName);
+        testBook.removeRecord(testName);
+        testBook.removeRecord(testName);
         int expected = 0;
         int actual = testBook.getAddressBook().size();
         Assert.assertEquals("Expects size to be 0", expected, actual);
@@ -92,7 +113,7 @@ public class PhoneBookTest {
 
     @Test
     public void removeTestNonExist(){
-        testBook.remove(testName2);
+        testBook.removeRecord(testName2);
         int expected = 1;
         int actual = testBook.getAddressBook().size();
         Assert.assertEquals("Expects size to be 1", expected, actual);
@@ -102,14 +123,14 @@ public class PhoneBookTest {
     @Test
     public void printNameList(){
         testBook.add(testName2, number2);
-        String expected = testName +"\n" +testName2;
+        String expected = testName +"\n" +testName2+"\n";
         String actual = testBook.printNameList();
         Assert.assertEquals("Expected Name list to return", expected, actual);
     }
 
     @Test
     public void printNameListEmpty(){
-        testBook.remove(testName);
+        testBook.removeRecord(testName);
         String expected = "List is empty";
         String actual = testBook.printNameList();
         Assert.assertEquals("Expected 'List is empty' return", expected, actual);
@@ -117,15 +138,15 @@ public class PhoneBookTest {
     @Test
     public void printEntryList(){
         testBook.add(testName2, number2);
-        String expected = testName +" : " + number + "\n"
-                            + testName2 + " : " + number2;
+        String expected = testName +" :\n " + number + "\n\n"
+                            + testName2 + " :\n " + number2 + "\n\n";
         String actual = testBook.printEntryList();
         Assert.assertEquals("Expected Entry list to return", expected, actual);
     }
 
     @Test
     public void printEntryListEmpty(){
-        testBook.remove(testName);
+        testBook.removeRecord(testName);
         String expected = "List is empty";
         String actual = testBook.printEntryList();
         Assert.assertEquals("Expected 'List is empty' return", expected, actual);
@@ -141,7 +162,7 @@ public class PhoneBookTest {
 
     @Test
     public void reverseLookUpTestEmpty(){
-        testBook.remove(testName);
+        testBook.removeRecord(testName);
         String expected = "List is empty";
         String actual = testBook.reverseLookUp(number);
         Assert.assertEquals("Expected 'List is empty' to return", expected, actual);
@@ -149,7 +170,7 @@ public class PhoneBookTest {
 
     @Test
     public void reverseLookUpMissingTest(){
-        String expected = "Name not found";
+        String expected = "Name not listed";
         String actual = testBook.reverseLookUp(number2);
         Assert.assertEquals("Expected 'Name not found' to return", expected, actual
         );
@@ -157,7 +178,7 @@ public class PhoneBookTest {
 
     @Test
     public void reverseLookUpTooLongTest(){
-        String expected = "Name not found";
+        String expected = "String > 32 characters";
         String actual = testBook.reverseLookUp(tooLong);
         Assert.assertEquals("Expected 'Name not found' to return", expected, actual
         );
