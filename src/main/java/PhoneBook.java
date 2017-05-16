cdbe2f7dbb1fe52901c76cf7a8a1bf1743d1b6a8
Brian Mullin <brianmullin@zipcoder6.local>
2017-05-16 17:28:06 -0400

    did some stuff

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,12 +1,33 @@
+import java.util.TreeMap;
+
 /**
  * Created by brianmullin on 5/16/17.
  */
 public class PhoneBook {
 
-    private PhoneBook(){}
+    private String name;
+    private String phoneNumber;
 
-    public static lookUp(){}
+    public PhoneBook(){}
 
-    public 
+    public static TreeMap<String, String> phoneBook = new TreeMap<String, String>();
+
+    public String lookUpByName(String name){
+
+        String entry= phoneBook.get(name);
+        return entry;
+    }
+
+    public void addEntries(String name, String phoneNumber){
+
+        phoneBook.put(name, phoneNumber);
+    }
+
+    public void removeEntries(){}
+
+    public String listAllEntries(){
+
+        return "";
+    }
 
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,5 +1,70 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.TreeMap;
+
 /**
  * Created by brianmullin on 5/16/17.
  */
+
 public class PhoneBookTest {
+
+    PhoneBook phoneBook;
+
+    @Before
+    public void setUp(){
+
+        phoneBook = new PhoneBook();
+        //public TreeMap<String, String> testPhoneBook = new TreeMap<String, String>();
+
+    }
+
+    @Test
+    public void lookUpByNameTest(){
+        //Given
+        String entry = "Brian";
+        String expectedNumber = "302-757-1634";
+
+        //When
+        phoneBook.put("Brian", "302-757-1634");
+        String actualNumber = phoneBook.lookUpByName("Brian");
+        //Then
+        Assert.assertEquals("The expected phone number is ", expectedNumber, actualNumber);
+
+
+    }
+
+    @Test
+    public void addEntriesTest(){
+        //Given
+        String name = "Brian";
+        String phoneNumber = "302-757-1634";
+
+        //When
+
+        //Then
+
+    }
+
+    @Test
+    public void removeEntriesTest(){
+        //Given
+
+        //When
+
+        //Then
+
+    }
+
+    @Test
+    public void listAllEntriesTest(){
+        //Given
+
+        //When
+
+        //Then
+
+    }
+
+
 }
