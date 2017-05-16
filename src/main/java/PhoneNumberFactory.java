725cb480010425cc1bcc75617c30b212ad14f88d
wongandrewp <wong.andrew.p@gmail.com>
2017-05-16 15:49:11 -0400

    Made classes, now writing tests

---
+++ src/main/java/PhoneNumberFactory.java
@@ -0,0 +1,5 @@
+/**
+ * Created by andrewwong on 5/16/17.
+ */
+public class PhoneNumberFactory {
+}
---
+++ src/test/java/PhoneBookTests.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by andrewwong on 5/16/17.
+ */
+public class PhoneBookTests {
+    PhoneBook phoneBook;
+    @Before
+    public void initPhoneBook(){
+        phoneBook = new PhoneBook();
+    }
+    @Test
+    public void testAdd(){
+        //Given
+        String name = "John Doe";
+        //When
+        //Then
+    }
+}
