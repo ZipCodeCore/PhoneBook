91aaf47e94e095849365efba176c2c8804f5f6ee
ant <anthonyjones@zipcoder3.local>
2017-05-16 16:47:33 -0400

    Added Test and more work in methods

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,46 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  * Created by anthonyjones on 5/16/17.
  */
 public class PhoneBook {
+
+    Map<String, String> phoneInfo = new HashMap<String, String>();
+    Scanner in = new Scanner(System.in);
+
+    public PhoneBook() {
+
+    }
+
+    //Originally going to be voided methods, I decided to return the entire hashmap after it's populated
+    public Map addEntry(String name, String phoneNumber) {
+
+        return phoneInfo;
+    }
+
+    //Originally going to be voided methods, I decided to return the entire hashmap after a entry is removed.
+    public Map removeEntry(String name) {
+       
+        return phoneInfo;
+    }
+
+    public void listName(String name) {
+    }
+
+    public void listAllEntries() {
+    }
+
+    //unsure how to populate Hashmap so I created  signature that takes in one and uses the putAll method.
+    public String lookUp(Map hashMap, String name) {
+        phoneInfo.putAll(hashMap);
+        return phoneInfo.get(name);
+    }
+
+    public String reverseLookUp(String number) {
+        return "";
+    }
+
+
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,5 +1,33 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Created by anthonyjones on 5/16/17.
  */
 public class PhoneBookTest {
+
+    @Test
+    public void lookUpTest() {
+        //Given
+        Map<String, String> phoneInfo = new HashMap<>();
+        phoneInfo.put("Sarah Silverman", "(302)-434-1849");
+        String id = "Sarah Silverman";
+        String expected = "(302)-434-1849";
+        PhoneBook phoneBook = new PhoneBook();
+        //When
+        String actual = phoneBook.lookUp(phoneInfo, id);
+        //Then
+        Assert.assertEquals("The number of Sarah is \"(302)-434-1849\" ", expected, actual);
+    }
+
+    @Test
+    public void
+
+
+
+
 }
