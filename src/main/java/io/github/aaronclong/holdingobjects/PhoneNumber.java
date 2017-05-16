0991fe6ea3cb62d458ee816d2d516aa4030b556d
Aaron Long <aaron.long.c@gmail.com>
2017-05-16 15:50:56 -0400

    Fixed format issues for check-style

--- src/main/java/io/github/aaronclong/holdingobjects/PhoneNumber.java
+++ src/main/java/io/github/aaronclong/holdingobjects/PhoneNumber.java
@@ -4,22 +4,22 @@ package io.github.aaronclong.holdingobjects;
  * Created by aaronlong on 5/16/17.
  */
 public class PhoneNumber {
-    private final int areaCode;
-    private final int centralOfficeCode;
-    private final int phoneLineCode;
+  private final int areaCode;
+  private final int centralOfficeCode;
+  private final int phoneLineCode;
 
-    private PhoneNumber(int theArea, int theOffice, int theLine) {
-        areaCode = theArea;
-        centralOfficeCode = theOffice;
-        phoneLineCode = theLine;
-    }
+  private PhoneNumber(int theArea, int theOffice, int theLine) {
+    areaCode = theArea;
+    centralOfficeCode = theOffice;
+    phoneLineCode = theLine;
+  }
 
-    public static PhoneNumber registerNumber(int areaCode, int centralOfficeCode, int phoneLineCode) {
-        return new PhoneNumber(areaCode, centralOfficeCode, phoneLineCode);
-    }
+  static PhoneNumber registerNumber(int areaCode, int centralOfficeCode, int phoneLineCode) {
+    return new PhoneNumber(areaCode, centralOfficeCode, phoneLineCode);
+  }
 
-    @Override
-    public String toString() {
-        return String.format("(%d)-%d-%d", areaCode, centralOfficeCode, phoneLineCode);
-    }
+  @Override
+  public String toString() {
+    return String.format("(%d)-%d-%d", areaCode, centralOfficeCode, phoneLineCode);
+  }
 }
