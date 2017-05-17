2ed2371a65735692d3d91756175a2a89b27d0a19
vidyachandasekhar <vidyahari.vs@Gmail.com>
2017-05-16 23:25:02 -0400

    >working on part III

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,39 +1,68 @@
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
+ *
+ Some people have more than one phone number. Refactor your PhoneBook class to map names to lists of phone numbers.
+ You should modify your add() and remove() methods to handle adding or removing individual numbers, and create a
+ removeRecord method for removing an entire entry from your PhoneBook.
+
+
  * Created by vidyachandasekhar on 5/16/17.
  */
 public class PhoneBook {
 
 
-    private Map<String, String> phoneMap = new TreeMap();
+    private Map<String, Set<String>> phoneMap = new TreeMap();
     private Map<String, String> reversePhoneMap = new HashMap();
 
 
     public void addEntry(String name, String number) {
-        phoneMap.put(name, number);
+        if (phoneMap.get(name) == null){
+            phoneMap.put(name, new HashSet());
+        }
+        Set phoneNumberSet = phoneMap.get(name);
+        phoneNumberSet.add(number);
         reversePhoneMap.put(number,name);
 
     }
 
     public void deleteEntry(String name) {
-        String numberToBeDeleted = phoneMap.get(name);
+        Set<String> phoneNumberSet = phoneMap.get(name);
+        if (phoneNumberSet == null){
+            return;
+        }
         phoneMap.remove(name);
-        reversePhoneMap.remove(numberToBeDeleted);
+        for (String phoneNumber: phoneNumberSet){
+            reversePhoneMap.remove(phoneNumber);
+        }
 
     }
 
     public String lookUpName(String name) {
-        return phoneMap.get(name);
+        Set<String>  phoneNumberSet =phoneMap.get(name);
+        return   getPhoneNumber(phoneNumberSet);
+    }
+
+    private String getPhoneNumber(Set<String> phoneNumberSet){
+        if (phoneNumberSet == null){
+            return null;
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String phoneNumber : phoneNumberSet){
+            stringBuilder.append(" "+ phoneNumber +" ");
+        }
+        return stringBuilder.toString().trim();
     }
 
     public String lookUpAllEntries() {
 
         StringBuilder stringBuilder = new StringBuilder();
-        for (Map.Entry<String, String> entry : phoneMap.entrySet()) {
-            stringBuilder.append(("Name : " + entry.getKey() + " Phone Number : " + entry.getValue() + "\n"));
+        for (Map.Entry<String, Set<String>> entry : phoneMap.entrySet()) {
+            stringBuilder.append(("Name : " + entry.getKey() + " Phone Number : " + getPhoneNumber(entry.getValue()) + "\n"));
         }
         return stringBuilder.toString();
     }
