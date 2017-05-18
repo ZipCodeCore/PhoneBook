c11d778efecf59503c4710dc58d543f76e3ac8bc
Molly Arant <mccormick.mollyt@gmail.com>
2017-05-18 19:14:39 -0400

    completed holding objects lab part 3

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,6 @@
 
 
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -11,50 +12,74 @@ public class PhoneBook {
     private String contactName;
     private String phoneNumber;
 
-    private TreeMap<String, String> contactInfo= new TreeMap<String, String>();
+   private TreeMap<String, ArrayList<String>> contactInfo= new TreeMap();
 
-    public TreeMap<String, String> getContactInfo() {
+    public TreeMap<String, ArrayList<String>> getContactInfo() {
         return contactInfo;
     }
 
-    public void setContactInfo(TreeMap<String, String> contactInfo) {
-        this.contactInfo = contactInfo;
-    }
 
 
-
-    public String lookup(String contactName){
+    public ArrayList<String> lookup(String contactName, TreeMap<String, ArrayList<String>> contactInfo ){
         return contactInfo.get(contactName);
     }
 
-    public void addEntry(String contactName, String phoneNumber){
-            contactInfo.put(contactName, phoneNumber);
+
+    public TreeMap addEntry(String contactName, String phoneNumber, TreeMap<String, ArrayList<String>> contactInfo){
+            if (doesNumberExistInContactInfo(contactName,contactInfo)){
+                contactInfo.get(contactName).add(phoneNumber);
+            }
+            else{
+                addNewEntry(contactName, phoneNumber, contactInfo);
+            }
+            return contactInfo;
     }
 
-    public void removeEntry(String contactName){
+
+
+    public void removeRecord(String contactName, TreeMap<String, ArrayList<String>> contactInfo){
         contactInfo.remove(contactName);
 
     }
 
-    public String listAllEntries(){
+    public void removeNumber(String contactName, String phoneNumber, TreeMap<String, ArrayList<String>> contactInfo){
+        if(doesNumberExistInContactInfo(contactName,contactInfo)){
+            contactInfo.get(contactName).remove(contactInfo.get(contactName).indexOf(phoneNumber));
+        }
+        else {
+            System.out.println("The number does not exist");
+        }
+
+    }
+
+
+
+    public String listAllEntries(TreeMap<String, ArrayList<String>> contactInfo){
         String allEntries = "";
-        for (Map.Entry<String, String> entry : contactInfo.entrySet()) {
+        for (Map.Entry<String, ArrayList<String>> entry : contactInfo.entrySet()) {
             allEntries+= ("\n"+ "Name: " + entry.getKey() + " Phone Number: " + entry.getValue());
         }
 
         return allEntries.trim();
     }
 
-    public String listAllNames(){
+
+    public String listAllNames(TreeMap<String, ArrayList<String>> contactInfo){
         String allEntries= "";
-        for (Map.Entry<String, String> entry: contactInfo.entrySet()){
+        for (Map.Entry<String, ArrayList<String>> entry: contactInfo.entrySet()){
             allEntries+=("\n"+ "Name: " + entry.getKey());
         }
         return allEntries.trim();
     }
 
+    private boolean doesNumberExistInContactInfo(String contactName, TreeMap<String, ArrayList<String>> contactInfo){
+        return contactInfo.containsKey(contactName);
+    }
 
-
-
+    private void addNewEntry(String contactName, String phoneNumber, TreeMap<String, ArrayList<String>> contactInfo){
+        ArrayList<String> numbers= new ArrayList<String>();
+        numbers.add(phoneNumber);
+        contactInfo.put(contactName, numbers);
+    }
 
 }
--- src/test/java/TestPhonebook.java
+++ src/test/java/TestPhonebook.java
@@ -1,6 +1,11 @@
+import com.sun.deploy.util.StringUtils;
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
 
 /**
  * Created by mollyarant on 5/16/17.
@@ -17,62 +22,89 @@ public class TestPhonebook {
 
 
         //given:
+
         String contactName = "Molly";
         String phoneNumber= "(302)-555-9743";
+        ArrayList<String> expected= new ArrayList<String>();
+        expected.add("(302)-555-9743");
 
         //when:
-        phoneBook.addEntry(contactName, phoneNumber);
+        phoneBook.addEntry(contactName, phoneNumber, phoneBook.getContactInfo());
 
         //then:
-        Assert.assertEquals("Should add a new entry to the phonebook","(302)-555-9743", phoneBook.getContactInfo().get("Molly"));
+        Assert.assertEquals("Should add a new entry to the phonebook",expected, phoneBook.lookup(contactName, phoneBook.getContactInfo()));
     }
-
+//
     @Test
-    public void testRemoveEntry(){
+    public void testRemoveRecord(){
         //given:
         String contactName = "Molly";
         String phoneNumber= "(302)-555-9743";
-        phoneBook.addEntry(contactName, phoneNumber);
+        phoneBook.addEntry(contactName, phoneNumber, phoneBook.getContactInfo());
         //when:
-        phoneBook.removeEntry("Molly");
+        phoneBook.removeRecord("Molly", phoneBook.getContactInfo());
 
         //then:
         Assert.assertEquals("should return null if the key has been removed",null, phoneBook.getContactInfo().get("Molly"));
 
 
     }
+    @Test
+    public void testRemoveEntry(){
+        //given:
+        TreeMap<String, ArrayList<String>> contactInfo= phoneBook.getContactInfo();
+        String contactName = "Molly";
+        String number1="3025555555";
+        String number2="3024444444";
+        ArrayList<String> expected = new ArrayList<String>();
+        expected.add(number2);
+        phoneBook.addEntry(contactName, number1, contactInfo);
+        phoneBook.addEntry(contactName, number2, contactInfo);
+
+        //when:
+        phoneBook.removeNumber("Molly", number1, phoneBook.getContactInfo());
+        //then:
+        Assert.assertEquals("Should return only one phone number", expected, phoneBook.getContactInfo().get("Molly"));
+    }
+
 
     @Test
     public void testLookUp(){
         //given:
+        TreeMap<String, ArrayList<String>> contactInfo= phoneBook.getContactInfo();
         String contactName = "Molly";
-        String phoneNumber= "(302)-555-9743";
-        phoneBook.addEntry(contactName, phoneNumber);
+        String number1="3025555555";
+        String number2="3024444444";
+        ArrayList<String> expected = new ArrayList<String>();
+        expected.add(number1);
+        expected.add(number2);
+        phoneBook.addEntry(contactName, number1, contactInfo);
+        phoneBook.addEntry(contactName, number2, contactInfo);
         //when:
-        phoneBook.lookup("Molly");
+        phoneBook.lookup("Molly", contactInfo);
 
         //then:
-        Assert.assertEquals("should return the phone number attached to the key","(302)-555-9743", phoneBook.lookup("Molly"));
 
-
+        Assert.assertEquals("should return the phone number attached to the key",expected, phoneBook.lookup("Molly", contactInfo));
     }
+
 
     @Test
     public void testListAllEntries(){
         //given:
         String contactName = "Molly";
         String phoneNumber= "(302)-555-9743";
-        phoneBook.addEntry(contactName, phoneNumber);
+        phoneBook.addEntry(contactName, phoneNumber,phoneBook.getContactInfo());
         String contactName2= "Steven";
         String phoneNumber2= "(302)-555-2456";
-        phoneBook.addEntry(contactName2,phoneNumber2);
-        String expected= "Name: Molly Phone Number: (302)-555-9743\n" +
-                "Name: Steven Phone Number: (302)-555-2456";
+        phoneBook.addEntry(contactName2,phoneNumber2, phoneBook.getContactInfo());
+        String expected= "Name: Molly Phone Number: [(302)-555-9743]\n" +
+                "Name: Steven Phone Number: [(302)-555-2456]";
         //when:
-        phoneBook.listAllEntries();
+        phoneBook.listAllEntries(phoneBook.getContactInfo());
 
         //then:
-        Assert.assertEquals("Should return all info in the phonebook",expected, phoneBook.listAllEntries());
+        Assert.assertEquals("Should return all info in the phonebook",expected, phoneBook.listAllEntries(phoneBook.getContactInfo()));
 
 
     }
@@ -82,17 +114,17 @@ public class TestPhonebook {
         //given:
         String contactName = "Molly";
         String phoneNumber= "(302)-555-9743";
-        phoneBook.addEntry(contactName, phoneNumber);
+        phoneBook.addEntry(contactName, phoneNumber, phoneBook.getContactInfo());
         String contactName2= "Steven";
         String phoneNumber2= "(302)-555-2456";
-        phoneBook.addEntry(contactName2,phoneNumber2);
+        phoneBook.addEntry(contactName2,phoneNumber2, phoneBook.getContactInfo());
         String expected= "Name: Molly\n" +
                 "Name: Steven";
         //when:
-        phoneBook.listAllNames();
+        phoneBook.listAllNames(phoneBook.getContactInfo());
 
         //then:
-        Assert.assertEquals("should return the list of names",expected, phoneBook.listAllNames());
+        Assert.assertEquals("should return the list of names",expected, phoneBook.listAllNames(phoneBook.getContactInfo()));
 
 
     }
