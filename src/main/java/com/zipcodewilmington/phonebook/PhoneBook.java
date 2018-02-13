27e5ec9958bc1b4d00fd3d26f3f5d346b6160bdd
Jessica Campbell <jessicacampbell@zipcoders-MacBook-Pro-42.local>
2018-02-12 20:21:11 -0500

    first

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,34 @@
 package com.zipcodewilmington.phonebook;
 
+import apple.laf.JRSUIUtils;
+import com.sun.tools.corba.se.idl.StringGen;
+
+import java.util.TreeMap;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
-}
+
+    private TreeMap<String, String> tmap;
+
+   PhoneBook(){
+
+      this.tmap =new TreeMap<String, String>();
+
+   }
+
+   public void addAContact(String name, String number) {
+       this.tmap.put(name, number);
+   }
+
+   public void removeAContact(String name, String number){
+       this.tmap.remove(name, number);
+   }
+    public void lookupContact (String name, String number){
+
+    }
+
+
+       }
+
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,21 @@
 package com.zipcodewilmington.phonebook;
 
+import org.junit.Test;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+
+    @Test
+    public void firstContactName() {
+
+    }
+
+
+
+
+
+
+
 }
