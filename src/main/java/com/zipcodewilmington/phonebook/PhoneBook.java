a0a61f6779cab9e5c9fd283c0959de2a7501a552
Kibret Tecle <kibrettecle@zipcoders-MacBook-Pro-2.local>
2018-02-13 08:19:09 -0500

    Still working on the test cases

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,72 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    Map<String,String> phoneBook = new TreeMap<String, String>();
+    Set<String>names;
+    Set<String>phoneBookList;
+    String nameLists="";
+    String namesAndNumbers ="";
+
+
+    public PhoneBook(Map<String, String> phoneBook) {
+        this.phoneBook = phoneBook;
+    }
+
+    public PhoneBook() {
+
+    }
+
+    public void add(String name,String phoneNumber) {
+
+     phoneBook.put(name, phoneNumber);
+    }
+
+    public void remove(String name){
+        if(phoneBook.containsKey(name)){
+            phoneBook.remove(name);
+        }
+        else{
+            System.out.println(name+" does not exist in the phone book list");
+        }
+
+    }
+
+        public String lookup(String name){
+
+        return phoneBook.get(name);
+        }
+
+        public String listNames(){
+        if(phoneBook.isEmpty()){
+            return null;
+        }else {
+            Set<String> names = phoneBook.keySet();
+
+            for (String name : names) {
+                nameLists += name + "\n";
+            }
+            return nameLists;
+        }
+
+        }
+
+        public String listNamesAndNumbers(){
+        Set<String>PhoneBookList = phoneBook.keySet();
+            for (String nameKeys:phoneBookList) {
+                namesAndNumbers+=nameKeys + "   "+phoneBook.get(nameKeys)+ "\n";
+            }
+
+        return namesAndNumbers;
+        }
+
+
+
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,48 @@
 package com.zipcodewilmington.phonebook;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * Created by leon on 1/23/18.
  */
+
+
+
+
 public class PhoneBookTest {
+    private static PhoneBook phoneBookRecord;
+
+    @Before
+    public void setUp() {
+        //PhoneBook phoneBookRecord;
+        Map<String,String>initialRecord = new TreeMap<String, String>(){ {put("John","1234567890");put("Micheal","5678901234");
+            put("James","1234554321");}};
+        this.phoneBookRecord=new PhoneBook(initialRecord);
+
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testLookup() {
+        String expected = "5678901234";
+        String actual = phoneBookRecord.lookup("Micheal");
+        Assert.assertEquals(expected, actual);
+    }
+    public void testlistNames(){
+        String expected = "John\n" +
+                            "Micheal\n"+
+                            "James\n";
+        String actual = phoneBookRecord.listNames();
+        Assert.assertEquals(expected, actual);
+
+    }
 }
