a845225d2e8501662116d0dd57bf2f69dbaf1d35
Haysel Santiago <hayselsantiago@zipcoders-MacBook-Pro-7.local>
2018-02-13 08:06:11 -0500

    Still being heavily worked on

--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -4,4 +4,9 @@ package com.zipcodewilmington.phonebook;
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+
+    private static String [] stringArray;
+    private static volatile
+
+
 }
