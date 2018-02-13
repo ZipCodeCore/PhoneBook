f0146f78a9a37789e13929c8a7dd9100d9abc635
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-02-12 19:56:47 -0500

    Not sure if previous went through but this is up to part 2

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,56 @@
 package com.zipcodewilmington.phonebook;
 
-/**
- * Created by leon on 1/23/18.
- */
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.ArrayList;
+
 public class PhoneBook {
+
+    TreeMap<String, String> phoneBookList = new TreeMap<String, String>();
+
+    public void addEntry(String name, String number) {
+        phoneBookList.put(name, number);
+        //Look to see if person exists
+        //Get array list value
+    }
+
+    public void removeEntry(String name, String number) {
+        phoneBookList.remove(name, number);
+    }
+
+    public String lookUp(String name) {
+        return phoneBookList.get(name);
+    }
+
+    public String listNames() {
+        return phoneBookList.keySet().toString();
+    }
+
+    public String listAll() {
+
+        StringBuilder sb = new StringBuilder();
+        for(Map.Entry<String, String> entry : phoneBookList.entrySet()) {
+            sb.append(entry.getKey() + entry.getValue());
+            sb.append("\n");
+        }
+        return sb.toString();
+
+    }
+
+    public String reverseLookUp(String number) {
+        for(Map.Entry<String, String>entry : phoneBookList.entrySet()) {
+            if(entry.getValue() == number) {
+                return entry.getKey();
+            }
+        }
+        return "That number is not in the phone book";
+    }
+
+    public static void main(String[] args) {
+        PhoneBook pb1 = new PhoneBook();
+        pb1.addEntry("Brian", "12345678");
+        pb1.addEntry("Lawrence", "23458984");
+        pb1.addEntry("Anthony", "12381927");
+
+    }
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,110 @@
 package com.zipcodewilmington.phonebook;
 
-/**
- * Created by leon on 1/23/18.
- */
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class PhoneBookTest {
+
+    private static PhoneBook phoneBookTest;
+
+    @Before
+    public void createPhoneBook() {
+        this.phoneBookTest = new PhoneBook();
+    }
+
+    @Test
+    public void testAddEntry() {
+        String name = "Person 1";
+        String number = "1234567";
+        phoneBookTest.addEntry(name, number);
+        String expected = number;
+        String actual = phoneBookTest.lookUp(name);
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testAddEntry2() {
+        String name = "Person 2";
+        String number = "87654321";
+        phoneBookTest.addEntry(name, number);
+        String expected = number;
+        String actual = phoneBookTest.lookUp(name);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeEntryTest() {
+        String name = "Person 1";
+        String number = "8942418";
+        phoneBookTest.removeEntry(name, number);
+        String expected = null;
+        String actual = phoneBookTest.lookUp(name);
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void removeEntryTest2() {
+        String name = "Person 2";
+        String number = "12387874";
+        phoneBookTest.removeEntry(name, number);
+        String expected = null;
+        String actual = phoneBookTest.lookUp(name);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void listNamesTest() {
+        String name = "Person 1";
+        String number = "1234567";
+        phoneBookTest.addEntry(name, number);
+        String name2 = "Person 2";
+        String number2 = "2345678";
+        phoneBookTest.addEntry(name2, number2);
+        String name3 = "Person 3";
+        String number3 = "34567898";
+        phoneBookTest.addEntry(name3, number3);
+        String expected = "[Person 1, Person 2, Person 3]";
+        String actual = phoneBookTest.listNames();
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void listAllTest() {
+        String name = "Person 1";
+        String number = "1234567";
+        phoneBookTest.addEntry(name, number);
+        String name2 = "Person 2";
+        String number2 = "2345678";
+        phoneBookTest.addEntry(name2, number2);
+        String name3 = "Person 3";
+        String number3 = "34567898";
+        phoneBookTest.addEntry(name3, number3);
+        String expected = "Person 11234567\nPerson 22345678\nPerson 334567898\n";
+        String actual = phoneBookTest.listAll();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void reverseLookUpTest() {
+        String name = "Person 1";
+        String number = "1234567";
+        phoneBookTest.addEntry(name, number);
+        String expected = "Person 1";
+        String actual = phoneBookTest.reverseLookUp(number);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void reverseLookUpTest2() {
+        String name = null;
+        String number = null;
+        String expected = "That number is not in the phone book";
+        String actual = phoneBookTest.reverseLookUp(number);
+        Assert.assertEquals(expected, actual);
+
+    }
 }
