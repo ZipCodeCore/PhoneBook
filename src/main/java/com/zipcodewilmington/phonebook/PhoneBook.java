e3328a39c507bdb2ca835e6e5515c918d8172680
April Rivera <aprilrivera@zipcoders-MBP-2.fios-router.home>
2018-02-13 06:59:14 -0500

    first commit, need to finish tests

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,63 @@
 package com.zipcodewilmington.phonebook;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashMap;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+
+    String name;
+    String number;
+
+
+    private Map<String, String> contactsList;
+
+    public PhoneBook(){
+        this.contactsList = new HashMap<String, String>();
+        contactsList.put("Pete Jones", "2678849087");
+        contactsList.put("Ron Burgundy", "5604329932");
+        contactsList.put("Nala Bits", "2345578122");
+
+    }
+    public Map<String, String> getContactsList(){
+        return contactsList;
+    }
+
+    public void addContact(String name, String number) {
+        if (!contactsList.containsKey(name)) {
+            contactsList.put(name, number);
+            System.out.println("New contact " + name + " has been added");
+        } else {
+            System.out.println("Contact already exists");
+        }
+    }
+
+    public void removeContact(String name) {
+        if (contactsList.containsKey(name)) {
+            contactsList.remove(name);
+            System.out.println("Contact has been removed");
+        } else {
+            System.out.println("No " + name + " contact exists");
+        }
+    }
+
+    public String lookUpContactByName(String name){
+        return contactsList.get(name) == null ? "Sorry name not found" : contactsList.get(name);
+    }
+
+    public void listAllByName(String name){
+        for(String ContactsByName : contactsList.keySet()){
+            System.out.println(contactsList.get(name));
+        }
+
+    }
+    public void listAllNamesAndNumbers(){
+        Set<Map.Entry<String, String>>listingAll = contactsList.entrySet();
+        System.out.println(contactsList);
+    }
+
+}
+
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -4,4 +4,40 @@ package com.zipcodewilmington.phonebook;
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+
+
+    PhoneBook phoneBook;
+
+    @Before
+    public void setUp(){
+        phoneBook = new PhoneBook();
+
+    }
+    @Test
+    public void testAddContact(){
+        phoneBook.addContact("Genya", "6578356767");
+        boolean genyaIsInPhoneBook = phoneBook.getContactsList().keySet().contains("Genya");
+        assertTrue(genyaIsInPhoneBook);
+
+    }
+    @Test
+    public void testRemoveContact(){
+        phoneBook.removeContact("Pete Jones");
+        boolean peteJonesHasBeenRemoved = !phoneBook.getContactsList().containsKey("Pete Jones");
+        assertTrue(peteJonesHasBeenRemoved);
+    }
+    @Test
+    public void testLookUpContactByName(){
+        phoneBook.lookUpContactByName("Nala Bits");
+        String expected = "2345578122";
+        String actual = phoneBook.getContactsList().get("Nala Bits");
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testListAllByName(){
+
+    }
+
+}
+
 }
