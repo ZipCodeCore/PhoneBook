2ab609959595f146831f6aabce15f99376571009
Owen Murphy <owenmurphy@zipcoders-MacBook-Pro-3.local>
2018-02-13 14:52:23 -0500

    Phone Home

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,48 @@
 package com.zipcodewilmington.phonebook;
-
+import java.util.Set;
+import java.util.TreeMap;
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    TreeMap<String, String> myTree = new TreeMap<String, String>();
+
+    public PhoneBook (){
+
+    }
+
+    public void add(String name, String number){
+        myTree.put(name, number);
+    }
+
+    public void remove(String name){
+        myTree.remove(name);
+    }
+
+    public String lookup(String name){
+        return myTree.get(name);
+    }
+
+    public String listNames(){
+        String list = "";
+        Set<String> keys = myTree.keySet();
+        for (String i : keys){
+            list += i + ":" + myTree.get(i)+"\n";
+        }
+        return list;
+
+    }
+
+    public static void main(String[] args) {
+        PhoneBook myPhoneBook = new PhoneBook();
+
+        myPhoneBook.add(name: "", number: "");
+        myPhoneBook.add(name: "", number: "");
+        myPhoneBook.add(name: "", number: "");
+        myPhoneBook.add(name: "", number: "");
+
+        System.out.println(myPhoneBook. listNamesAndNumbers());
+
+
+    }
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,60 @@
 package com.zipcodewilmington.phonebook;
-
+import org.junit.Assert;
+import org.junit.Test;
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+
+    @Test
+    public void addTest1() {
+        PhoneBook testPhoneBook = new PhoneBook();
+        testPhoneBook.add(name: "", number: "");
+        String testNumber = testPhoneBook.lookup(name:);
+        Assert.assertFalse(testNumber.equals(""));
+    }
+
+    @Test
+    public void addTest2() {
+                    PhoneBook testPhoneBook = new PhoneBook();
+        PhoneBook testPhoneBook = new PhoneBook();
+        testPhoneBook.add ("", "");
+        String testNumber = testPhoneBook.lookup ("");
+        Assert.assertFalse(testNumber.equals(""));
+
+
+    }
+    @Test
+    public void removeTest(){
+        PhoneBook testPhoneBook = new PhoneBook();
+    }
+
+    @Test
+    public void lookupTest1(){
+        PhoneBook testPhoneBook = new PhoneBook();
+        testPhoneBook.add("", "");
+        String testNumber = testPhoneBook.lookup("");
+        Assert.assertFalse(testNumber.equals(""));
+    }
+    @Test
+    public void lookupTest2(){
+        PhoneBook testPhoneBook = new PhoneBook();
+        testPhonebook.add("", "");
+        String testNumber = testPhoneBook.lookup("");
+        Assert.assertFalse(testNumber.equals(""));
+    }
+    @Test
+    public void listNamesTest() {
+        PhoneBook testPhoneBook = new PhoneBook();
+        testPhoneBook.add("", "");
+        testPhoneBook.add("", "");
+        testPhoneBook.add("", "");
+        testPhoneBook.add("", "");
+    }
+    @Test
+    public void listNamesAndNumbersTest(){
+
+        PhoneBook testPhoneBook = new PhoneBook();
+    }
+
 }
