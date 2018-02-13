0345cfbc7747f0eac683ee49e7c150be95c85520
Joe Hendricks <joehendricks@zipcoders-MacBook-Pro-92.local>
2018-02-12 23:46:38 -0500

    The birth of a phonebook

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,91 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    private String name;
+    private String number;
+
+    TreeMap<String, String> nameNumberBook = new TreeMap<String, String>();
+
+    public PhoneBook(){
+
+    }
+
+    public PhoneBook(String name, String number){
+        this.name = name;
+        this.number = number;
+
+    }
+
+    public void add(String name, String number){
+
+        nameNumberBook.put(name, number);
+    }
+
+    public void remove(String name){
+
+        nameNumberBook.remove(name);
+    }
+
+    public String lookup(String name){
+
+        return nameNumberBook.get(name);
+    }
+
+    public String listNames(){
+
+        Set<String> contacts = nameNumberBook.keySet();
+
+        String allContacts = "";
+        for(String name : contacts){
+            //System.out.println(name + "\n");
+            allContacts += name + "\n";
+        }
+        return allContacts;
+    }
+
+    public String listPhoneBook() {
+
+        String contactsAndNumbers = "";
+
+        for (Map.Entry<String, String> entry : nameNumberBook.entrySet()) {
+            String getKeys = String.format("%1$-15s", entry.getKey());
+            String getValues = String.format("%1$-15s", entry.getValue());
+
+            contactsAndNumbers += getKeys + getValues + "\n";
+        }
+
+        return contactsAndNumbers;
+    }
+
+    public static void main(String[] args) {
+
+        PhoneBook phonebook = new PhoneBook();
+
+        phonebook.add("George", "123.456.7890");
+        phonebook.add("Pete", "292.349.8023");
+        phonebook.add("Dan", "854.930.2175");
+        phonebook.add("Brad", "609.489.2345");
+        phonebook.add("Stan", "346.256.1765");
+        phonebook.add("Luke", "367.945.3984");
+
+
+
+        //phonebook.lookup("George");
+        //phonebook.remove("George");
+
+        //System.out.println(phonebook.lookup("George"));
+        //System.out.println(phonebook.listNames());
+        System.out.println(phonebook.listPhoneBook());
+
+
+
+    }
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,74 @@
 package com.zipcodewilmington.phonebook;
 
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/23/18.
  */
+
 public class PhoneBookTest {
+
+    @Test
+    public void addTest1() {
+        PhoneBook testBook = new PhoneBook();
+        testBook.add("George", "1234567890");
+        String testNumber = testBook.lookup("George");
+        Assert.assertTrue(testNumber.equals("1234567890"));
+    }
+
+    @Test
+    public void addTest2() {
+        PhoneBook testBook = new PhoneBook();
+        testBook.add("George", "1234567890");
+        String testNumber = testBook.lookup("George");
+        Assert.assertFalse(testNumber.equals("1234567891"));
+    }
+
+    @Test
+    public void lookUpTest1() {
+        PhoneBook testBook = new PhoneBook();
+        testBook.add("George", "1234567890");
+        String testNumber = testBook.lookup("George");
+        Assert.assertTrue(testNumber.equals("1234567890"));
+    }
+
+    @Test
+    public void lookUpTest2() {
+        PhoneBook testBook = new PhoneBook();
+        testBook.add("George", "1234567890");
+        String testNumber = testBook.lookup("George");
+        Assert.assertFalse(testNumber.equals("1234567891"));
+    }
+
+    @Test
+    public void removeTest1() {
+        PhoneBook testBook = new PhoneBook();
+        testBook.remove("Billy");
+        String testNumber = testBook.lookup("Billy");
+        Assert.assertEquals(null, testNumber);
+    }
+
+    @Test
+    public void removeTest2() {
+        PhoneBook testBook = new PhoneBook();
+        testBook.remove("George");
+        String testNumber = testBook.lookup("George");
+        Assert.assertEquals(null, testNumber);
+    }
+
+    /*@Test
+    public void listNamesTest() {
+        PhoneBook testBook = new PhoneBook();
+        testBook.add("George", "123.456.7890");
+        testBook.add("George", "123.456.7890");
+        String testNumber = testBook.lookup("George         123.456.7890" + "\n");
+        Assert.assertTrue(testNumber.equals("1234567890"));
+    }
+    */
+
+
+
 }
