821ec49e71ca1ef60c3d3ca7111a8b3f7def8980
Garrett Arant <garrettarant@MacBook-Pro-4.fios-router.home>
2018-02-12 22:31:31 -0500

    Garret Part 1 and 2 Done

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,71 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+
+    private String name;
+    private String number;
+    public TreeMap<String, String> phoneBookEntries = new TreeMap<String, String>();
+
+
+    public PhoneBook() {
+
+    }
+
+    public void add(String name, String number) {
+        phoneBookEntries.put(name, number);
+    }
+
+    public void remove(String name) {
+        phoneBookEntries.remove(name).toString();
+    }
+
+    public String lookUp(String name) {
+        return phoneBookEntries.get(name);
+
+    }
+
+    public String listNames() {
+
+        Set<String> contacts = phoneBookEntries.keySet();
+        String allContatcts = "";
+
+        for (String name : contacts) {
+            allContatcts += name + "\n";
+        }
+
+        return allContatcts;
+
+    }
+
+    public String listNamesAndNumbers() {
+
+        Set<String> contacts = phoneBookEntries.keySet();
+        String allContacts = "";
+
+        for (String name : contacts) {
+            allContacts += name + " " + phoneBookEntries.get(name) + "\n";
+        }
+
+        return allContacts;
+    }
+
+        public  String reverseLookUp(String value) {
+
+            Set<String> contacts = phoneBookEntries.keySet();
+            for(String name: contacts){
+                if(phoneBookEntries.get(name).equals(value)){
+                    return name;
+                }
+
+            }
+            return null;
+
+    }
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,89 @@
 package com.zipcodewilmington.phonebook;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+
+    PhoneBook testBook;
+
+    @Before
+    public void setUp(){
+         testBook = new PhoneBook();
+    }
+    @Test
+    public void testAdd() {
+
+        PhoneBook testBook = new PhoneBook();
+        testBook.add("George", "302-532-4531");
+        String testNumber = testBook.lookUp("George");
+        Assert.assertTrue(testNumber.equals("302-532-4531"));
+    }
+
+    @Test
+    public void testRemove() {
+
+        PhoneBook testBook = new PhoneBook();
+        testBook.add("George", "302-532-4531");
+        testBook.remove("George");
+        String testNumber = testBook.lookUp("George");
+        Assert.assertEquals("If you remove a phone number, it should return null",null, testNumber);
+    }
+
+    @Test
+    public void testLookUp() {
+
+        PhoneBook testBook = new PhoneBook();
+        testBook.add("George", "302-532-4531");
+        String testNumber = testBook.lookUp("George");
+        Assert.assertTrue(testNumber.equals("302-532-4531"));
+    }
+
+    @Test
+    public void testListNames() {
+
+        PhoneBook testBook = new PhoneBook();
+        testBook.add("George", "302-532-4531");
+        testBook.add("Bob", "659-234-6756");
+        testBook.add("Tony", "235-532-4531");
+        testBook.add("Paul", "454-532-4531");
+        testBook.add("Joe", "234-532-4531");
+        String actual= testBook.listNames();
+        String expected="Bob\nGeorge\nJoe\nPaul\nTony\n";
+        Assert.assertEquals(expected,actual);
+
+    }
+    @Test
+    public void testListNamesAndNumbers() {
+
+        PhoneBook testBook = new PhoneBook();
+        testBook.add("George", "302-532-4531");
+        testBook.add("Bob", "659-234-6756");
+        testBook.add("Tony", "235-532-4531");
+        testBook.add("Paul", "454-532-4531");
+        testBook.add("Joe", "234-532-4531");
+        String actual= testBook.listNamesAndNumbers();
+        String expected="Bob 659-234-6756\nGeorge 302-532-4531\nJoe 234-532-4531\nPaul 454-532-4531\nTony 235-532-4531\n";
+        Assert.assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void testReverseLookUp() {
+
+        PhoneBook testBook = new PhoneBook();
+        testBook.add("George", "302-532-4531");
+        testBook.add("Bob", "659-234-6756");
+        testBook.add("Tony", "235-532-4531");
+        testBook.add("Paul", "454-532-4531");
+        testBook.add("Joe", "234-532-4531");
+        String actual= testBook.reverseLookUp( "302-532-4531");
+        String expected="George";
+        Assert.assertEquals(expected,actual);
+
+    }
 }
