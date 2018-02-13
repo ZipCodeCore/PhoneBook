f16d77ee560440d1d5927b27fd7dbc3c3722493e
Katrice Williams-Dredden <katricewilliams-dredden@zipcoders-MacBook-Pro.local>
2018-02-13 08:14:23 -0500

    started not finished, confused

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -4,4 +4,55 @@ package com.zipcodewilmington.phonebook;
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+
+    private String name;
+    private String number;
+
+    //the instance of the phone book (ask about this, not 1000% clear
+    public Person(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+
+    //add number
+    public void addEntry() {
+
+        return null;
+    }
+
+    //remove number and name
+    public void removeContact(String name) {
+
+    }
+
+    //Look up entry
+    public void listAllNumbers() {
+        String[] listAllNumbers = new String[];
+        return;
+    }
+
+    //making a list of all entries
+    public String getphoneNumbers() {
+        return getphoneNumbers();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,28 @@
 package com.zipcodewilmington.phonebook;
-
+import org.junit.Test;
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+//test for adding an entry
+    @Test
+    public void test addEntry1() {
+        String expected = "856-774-4773";
+        PhoneBook.addEntry("Rodney", "856-774-4773");
+        String actual = PhoneBook.listAllNumbers("Rodney");
+
+        TestUtils.assertEquality(expected, actual);
+    }
+
+    @Test
+    public void test addEntry2() {
+        String expected = "856-774-4773";
+        PhoneBook.addEntry("Rodney", "856-774-4773");
+        String actual = PhoneBook.listAllNumbers("Rodney");
+
+        TestUtils.assertEquality(expected, actual);
+    }
+//
+
+
 }
