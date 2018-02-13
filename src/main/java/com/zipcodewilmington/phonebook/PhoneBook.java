91ce14f789bca091a8e6516a18abc9aca93de1c6
Daniel Horowitz <danielhorowitz@zipcoders-MacBook-Pro-80.local>
2018-02-12 19:15:53 -0500

    first pass w/o test cases

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,81 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+
+    private String name;
+    private String number;
+
+    TreeMap<String, String> myTree = new TreeMap<String, String>();
+
+
+    public PhoneBook(){
+
+    }
+
+//    public PhoneBook(String name, String number) {
+//        this.name = name;
+//        this.number = number;
+//
+//    }
+
+    public void addEntry(String name, String number) {
+        myTree.put(name, number);
+
+
+    }
+
+
+    public void remove(String name, String number) {
+        myTree.remove(name);
+
+    }
+
+    public String stringlookup(String name) {
+
+       return myTree.get(name);
+
+
+
+    }
+
+    public String listNames() {
+        ;
+        Set<String> contacts = myTree.keySet();
+
+        String allNames = "";
+        for (String contact : contacts) {
+            allNames += contact + "\n";
+
+        }
+        return allNames;
+    }
+
+    public String[] listPhoneBook() {
+
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+
+        PhoneBook phonebookEntry = new PhoneBook();
+
+        phonebookEntry.addEntry("Brian", "3027619121");
+        phonebookEntry.addEntry("Dan", "4846394190");
+        phonebookEntry.addEntry("Garret", "3027894586");
+
+        System.out.println(phonebookEntry.listNames());
+
+        phonebookEntry.stringlookup("Brian");
+
+        System.out.println(phonebookEntry.stringlookup("Dan"));
+    }
+
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,36 @@
 package com.zipcodewilmington.phonebook;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.theories.suppliers.TestedOn;
+
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+
+    @Before
+    public void setUp() {
+        testBook = new PhoneBook();
+    }
+
+
+
+
+    @Test
+    public void addTest() {
+
+        PhoneBook testBook = new PhoneBook();
+        String name = "Dan";
+        String number = "4847986543";
+       // String expected = "Dan 4847986543";
+
+        testBook.add(name, number);
+        Assert.assertEquals(expected, actual);
+    }
+
+
+
 }
