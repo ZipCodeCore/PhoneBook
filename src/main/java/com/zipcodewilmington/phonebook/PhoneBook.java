5fc439aba89443e036b872275e0d888a7619983e
mbowe4 <mlbowe@gmail.com>
2018-02-13 07:58:06 -0500

    first commit

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,41 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.HashMap;
+import java.util.TreeMap;
+
 /**
  * Created by leon on 1/23/18.
  */
+
+
 public class PhoneBook {
+
+    TreeMap<String, String> contacts;
+
+    public PhoneBook () {
+        contacts = new TreeMap();
+    }
+
+    public String addPerson(String name, String phoneNumber) {
+        return contacts.put(name, phoneNumber);
+    }
+
+    public String lookupPhoneNumber(String name) {
+        return contacts.get(name);
+    }
+
+    public String removePerson(String name) {
+        return contacts.remove(name);
+    }
+
+    public void  listNamesAndNums(String name, String phoneNumber) {
+
+        for (String key : contacts.keySet()){
+            String value = contacts.get(key);
+            System.out.println(key + ": " + value);
+        }
+
+    }
+
+
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,45 @@
 package com.zipcodewilmington.phonebook;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+
+    @Test
+    public void testConstructor() {
+        // Given
+        String expectedName = "";
+        String expectedPhoneNumber = "";
+
+        // When
+        Person person = new Person(expectedName, expectedPhoneNumber);
+
+        // Then
+        String actualName = person.getName();
+        String actualPhoneNumber = person.getPhoneNumber();
+
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedPhoneNumber, actualPhoneNumber);
+    }
+
+    @Test
+    public void testConstructorWithNameAndPhonNumber() {
+        // Given
+        String expectedName = "Leon";
+        String expectedPhoneNumber = "123";
+
+        // When
+        Person person = new Person(expectedName, expectedPhoneNumber);
+
+        // Then
+        String actualPhoneNumber = person.getPhoneNumber();
+        String actualName = person.getName();
+
+        Assert.assertEquals(expectedPhoneNumber, actualPhoneNumber);
+        Assert.assertEquals(expectedName, actualName);
+    }
 }
