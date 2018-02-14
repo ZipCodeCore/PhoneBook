edca2aa91fbf92d15f80ce95b1e6385b6ec65fa6
Bo Lee <bolee@zipcoders-MacBook-Pro-4.local>
2018-02-13 19:33:32 -0500

    Part 3 complete

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,4 +1,6 @@
 package com.zipcodewilmington.phonebook;
+import jdk.nashorn.internal.runtime.arrays.ArrayIndex;
+
 import java.util.*;
 
 /**
@@ -10,7 +12,7 @@ public class PhoneBook {
     //method 1
     private static final String NON_EXISTANT_MESSAGE = "This does not exist.";
 
-    private TreeMap<String, String> contacts;
+    private TreeMap<String, ArrayList<String>> contacts;
 
     public PhoneBook() {
         contacts = new TreeMap<>();
@@ -18,34 +20,52 @@ public class PhoneBook {
 
 
     public void add (String name, String phoneNumber) {
-        contacts.put(name, phoneNumber);
+        if (contacts.containsKey(name)){
+            contacts.get(name).add(phoneNumber);
+        }
+        else {
+            ArrayList<String> phoneNumbers = new ArrayList<>();
+            phoneNumbers.add(phoneNumber);
+            contacts.put(name , phoneNumbers);
+        }
     }
 
-    public String lookup(String name) {
+    public void remove(String phoneNumber) {
+        for(Map.Entry<String, ArrayList<String>> entry : contacts.entrySet()) {
+            ArrayList<String> phoneNumArrList = entry.getValue();
+            for (int i = 0; i < phoneNumArrList.size(); i++) {
+                if(phoneNumber.equals(phoneNumArrList.get(i))) {
+                    phoneNumArrList.remove(i);
+                }
+            }
+        }
+    }
+
+    public ArrayList<String> lookup(String name) {
         return contacts.get(name);
 
     }
 
-    public void remove (String name) {
+    public void removeRecord (String name) {
         contacts.remove(name);
     }
 
-    public String display() {
-        StringBuilder sb = new StringBuilder();
-        for(Map.Entry<String, String> entry : contacts.entrySet()) {
-            String name = entry.getKey();
-            String phoneNumber = entry.getValue();
-            sb.append(name + " " + phoneNumber + "\n");
-        }
-        return sb.toString();
-    }
+//    public String display() {
+//        StringBuilder sb = new StringBuilder();
+//        for(Map.Entry<String, String> entry : contacts.entrySet()) {
+//            String name = entry.getKey();
+//            String phoneNumber = entry.getValue();
+//            sb.append(name + " " + phoneNumber + "\n");
+//        }
+//        return sb.toString();
+//    }
     //
-    public String reverseLookUp (String phoneNumber){
-        for(Map.Entry<String, String> entry : contacts.entrySet()){
-            if (entry.getValue().equals(phoneNumber)) {
-                return entry.getKey();
-            }
-        }
-        return null;
-    }
+//    public String reverseLookUp (String phoneNumber){
+//        for(Map.Entry<String, String> entry : contacts.entrySet()){
+//            if (entry.getValue().equals(phoneNumber)) {
+//                return entry.getKey();
+//            }
+//        }
+//        return null;
+//    }
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -3,6 +3,7 @@ package com.zipcodewilmington.phonebook;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
@@ -12,105 +13,150 @@ import java.util.TreeMap;
  */
 public class PhoneBookTest {
 
-    @Test
-    public void lookup_ValidEntry_ReturnsNumber() {
-        // Arrange
-        String input = "215-333-4444";
-        String expected = "215-333-4444";
-
-        // Act
-        PhoneBook phoneBook = new PhoneBook();
-        phoneBook.add("theresa", input);
-        String actual = phoneBook.lookup("theresa");
-
-        // Assert
-        Assert.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void lookUp_NoEntry_ReturnsNull() {
-        PhoneBook phoneBook = new PhoneBook();
-        String actual = phoneBook.lookup("cody");
-        Assert.assertNull(actual);
-    }
-
-    @Test
-    public void add_AddsEntry() {
-        PhoneBook phoneBook = new PhoneBook();
-        String expected = "215-333-4444";
-        phoneBook.add("theresa", expected);
-        String actual = phoneBook.lookup("theresa");
-        Assert.assertEquals(expected, actual);
-    }
+//    @Test
+//    public void lookup_ValidEntry_ReturnsNumber() {
+//        // Arrange
+//        String input = "215-333-4444";
+//        String expected = "215-333-4444";
+//
+//        // Act
+//        PhoneBook phoneBook = new PhoneBook();
+//        phoneBook.add("theresa", input);
+//        String actual = phoneBook.lookup("theresa");
+//
+//        // Assert
+//        Assert.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void lookUp_NoEntry_ReturnsNull() {
+//        PhoneBook phoneBook = new PhoneBook();
+//        Assert.assertNull(actual);
+//    }
 
     @Test
-    public void remove_RemovesEntry() {
+    public void add_EntryDoesNotExists_CreatesNewArrayListAndAppendsNumber() {
         PhoneBook phoneBook = new PhoneBook();
         String expected = "215-333-4444";
-        phoneBook.add("theresa", expected);
-        phoneBook.remove("theresa");
-        String actual = phoneBook.lookup("theresa");
-        Assert.assertNull(actual);
-    }
-
-    @Test
-    public void display_HasEntry_ReturnsString() {
-        // Arrange
-        String input = "215-333-4444";
-        String expected = "theresa 215-333-4444\n";
-
-        // Act
-        PhoneBook phoneBook = new PhoneBook();
-        phoneBook.add("theresa", input);
-        String actual = phoneBook.display();
-
-        // Assert
-        Assert.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void display_HasMultipleEntry_ReturnsString() {
-        // Arrange
-        String expected = "min 215-222-4444\ntheresa 215-333-4444\n";
-
-        // Act
-        PhoneBook phoneBook = new PhoneBook();
         phoneBook.add("theresa", "215-333-4444");
-        phoneBook.add("min", "215-222-4444");
-
-        String actual = phoneBook.display();
-
-        // Assert
-        Assert.assertEquals(expected, actual);
+        String actual = phoneBook.lookup("theresa").get(0);
+        Assert.assertEquals(actual,expected);
     }
 
-
     @Test
-    public void reverseLookUp_EntryExists_ReturnsName() {
-        // Arrange
-        String expected = "min";
-
-        // Act
+    public void add_EntryExists_AppendsNumberToArrayList() {
         PhoneBook phoneBook = new PhoneBook();
+        String expected = "215-333-4444";
+        phoneBook.add("theresa", "215-333-5555");
         phoneBook.add("theresa", "215-333-4444");
-        phoneBook.add("min", "215-222-4444");
-
-        String actual = phoneBook.reverseLookUp("215-222-4444");
-
-        // Assert
-        Assert.assertEquals(expected, actual);
+        String actual = phoneBook.lookup("theresa").get(1);
+        Assert.assertEquals(actual,expected);
     }
 
     @Test
-    public void reverseLookUp_EntryDoesExists_ReturnsNull() {
-        // Arrange
-
-        // Act
+    public void remove_EntryExists_RemoveNumberToArrayList() {
         PhoneBook phoneBook = new PhoneBook();
 
-        String actual = phoneBook.reverseLookUp("215-222-4444");
+        phoneBook.add("theresa", "215-333-5555");
+        phoneBook.add("theresa", "215-333-4444");
+
+
+        phoneBook.remove("215-333-5555");
+        ArrayList<String> actual = phoneBook.lookup("theresa");
+        Assert.assertEquals(1, actual.size());
+    }
+
+    @Test
+    public void remove_MultipleEntryExists_RemoveNumberToArrayList() {
+        // Arrange
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("theresa", "215-333-5555");
+        phoneBook.add("theresa", "215-333-4444");
+        phoneBook.add("min", "215-333-5555");
+        phoneBook.add("min", "215-333-4444");
+        phoneBook.add("joe", "215-333-2141");
+
+        // Act
+        phoneBook.remove("215-333-5555");
 
         // Assert
-        Assert.assertNull(actual);
+        ArrayList<String> actual1 = phoneBook.lookup("theresa");
+        ArrayList<String> actual2 = phoneBook.lookup("min");
+        Assert.assertEquals(1, actual1.size());
+        Assert.assertEquals(1, actual2.size());
+
     }
+//
+//    @Test
+//    public void removeRecord_RemovesEntry() {
+//        PhoneBook phoneBook = new PhoneBook();
+//        String expected = "215-333-4444";
+//        phoneBook.add("theresa", expected);
+//        phoneBook.removeRecord("theresa");
+//        ArrayList<String> actual = phoneBook.lookup("theresa");
+//        Assert.assertNotNull(actual);
+//    }
+//
+//    @Test
+//    public void display_HasEntry_ReturnsString() {
+//        // Arrange
+//        String input = "215-333-4444";
+//        String expected = "theresa 215-333-4444\n";
+//
+//        // Act
+//        PhoneBook phoneBook = new PhoneBook();
+//        phoneBook.add("theresa", input);
+//        String actual = phoneBook.display();
+//
+//        // Assert
+//        Assert.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void display_HasMultipleEntry_ReturnsString() {
+//        // Arrange
+//        String expected = "min 215-222-4444\ntheresa 215-333-4444\n";
+//
+//        // Act
+//        PhoneBook phoneBook = new PhoneBook();
+//        phoneBook.add("theresa", "215-333-4444");
+//        phoneBook.add("min", "215-222-4444");
+//
+//        String actual = phoneBook.display();
+//
+//        // Assert
+//        Assert.assertEquals(expected, actual);
+//    }
+//
+//
+//    @Test
+//    public void reverseLookUp_EntryExists_ReturnsName() {
+//        // Arrange
+//        String expected = "min";
+//
+//        // Act
+//        PhoneBook phoneBook = new PhoneBook();
+//        phoneBook.add("theresa", "215-333-4444");
+//        phoneBook.add("min", "215-222-4444");
+//
+//        String actual = phoneBook.reverseLookUp("215-222-4444");
+//
+//        // Assert
+//        Assert.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void reverseLookUp_EntryDoesExists_ReturnsNull() {
+//        // Arrange
+//
+//        // Act
+//        PhoneBook phoneBook = new PhoneBook();
+//
+//        String actual = phoneBook.reverseLookUp("215-222-4444");
+//
+//        // Assert
+//        Assert.assertNull(actual);
+//    }
+//
+
 }
