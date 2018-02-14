366ac61639c22071ecbbc1f1cd19a93ccb2259de
Peter McCormick <petermccormick@zipcodersMBP109.fios-router.home>
2018-02-13 20:31:43 -0500

    Part 2 complete

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,5 +1,6 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.Set;
 import java.util.TreeMap;
 
 /**
@@ -7,29 +8,49 @@ import java.util.TreeMap;
  */
 public class PhoneBook {
 
-private String name;
-private String phoneNumber;
+    private String name;
+    private String phoneNumber;
+    private String phoneNumber2;
 
     private TreeMap<String, String> phoneBookDirectory = new TreeMap<String, String>();
 
 
     public String lookupNameToGetAccompanyingPhoneNumber(String name) {
-       return phoneBookDirectory.get(name);
+        return phoneBookDirectory.get(name);
+
 
     }
+
     public String addEntry(String name, String phoneNumber) {
         phoneBookDirectory.put(name, phoneNumber);
         return phoneBookDirectory.get(name);
 
     }
+
     public String removeEntry(String name) {
         phoneBookDirectory.remove(name);
-        return phoneBookDirectory.get(name);
+        return null;
+
     }
-    public java.util.Set listNames(){
+
+    public java.util.Set listNames() {
         return phoneBookDirectory.keySet();
     }
+
+
     public java.util.Set listNamesAndNumbers() {
         return phoneBookDirectory.entrySet();
+    }
+
+
+    public String reverseLookup(String phoneNumber) {
+        Set<String> names = phoneBookDirectory.keySet();
+        for (String key: names) {
+            if (phoneBookDirectory.get(key).contains(phoneNumber)) {
+                return key;
+            }
+        }
+        return null;
+
     }
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -22,27 +22,28 @@ public class PhoneBookTest {
     @Test
     public void testlookUpNameToGetAccompanyingPhoneNumber() {
         testPhoneBook.addEntry("Rodrigo", "233-123-3445");
-        testPhoneBook.addEntry("Eleina", "234-123-3344");
+        testPhoneBook.addEntry("Elena", "234-123-3344");
         testPhoneBook.addEntry("Robert", "332-233-7821");
         String expected = "332-233-7821";
         System.out.print(testPhoneBook.lookupNameToGetAccompanyingPhoneNumber("Robert"));
-        String actual =testPhoneBook.lookupNameToGetAccompanyingPhoneNumber("Robert");
-        Assert.assertEquals("should return Rodrigo's phone number", expected, actual);
+        String actual = testPhoneBook.lookupNameToGetAccompanyingPhoneNumber("Robert");
+        Assert.assertEquals("should return Robert's phone number", expected, actual);
 
     }
     @Test
     public void  testAddEntry() {
         String expected ="233-123-3445";
         String actual = testPhoneBook.addEntry("Rodrigo", "233-123-3445");
-        System.out.println(actual + expected);
+        System.out.println(actual + " " + expected);
         Assert.assertEquals(expected, actual);
     }
     @Test
     public void testRemoveEntry(){
         testPhoneBook.addEntry("Rodrigo", "233-123-3445");
-        testPhoneBook.addEntry("Eleina", "234-123-3344");
+        testPhoneBook.addEntry("Elena", "234-123-3344");
         testPhoneBook.addEntry("Robert", "332-233-7821");
-        String actual = testPhoneBook.removeEntry("Rodrigo");
+        String actual =testPhoneBook.removeEntry("Robert");
+        String name = "Robert";
         String expected = null;
         Assert.assertEquals(expected, actual);
 
@@ -50,7 +51,7 @@ public class PhoneBookTest {
     @Test
     public void testListNames() {
         testPhoneBook.addEntry("Rodrigo", "233-123-3445");
-        testPhoneBook.addEntry("Eleina", "234-123-3344");
+        testPhoneBook.addEntry("Elena", "234-123-3344");
         testPhoneBook.addEntry("Robert", "332-233-7821");
         System.out.println(testPhoneBook.listNames());
         }
@@ -58,10 +59,21 @@ public class PhoneBookTest {
     @Test
     public void testListNamesandNumbers() {
         testPhoneBook.addEntry("Rodrigo", "233-123-3445");
-        testPhoneBook.addEntry("Eleina", "234-123-3344");
+        testPhoneBook.addEntry("Elena", "234-123-3344");
         testPhoneBook.addEntry("Robert", "332-233-7821");
         System.out.println(testPhoneBook.listNamesAndNumbers());
     }
+
+    @Test
+    public void testReverseLookup() {
+        testPhoneBook.addEntry("Rodrigo", "233-123-3445");
+        testPhoneBook.addEntry("Elena", "234-123-3344");
+        testPhoneBook.addEntry("Robert", "332-233-7821");
+        String phoneNumber= "234-123-3344";
+        String expected = "Elena";
+        String actual = testPhoneBook.reverseLookup( "234-123-3344");
+        Assert.assertEquals("should return who the phone number is assigned to ", expected, actual);
+        }
 }
 
 
