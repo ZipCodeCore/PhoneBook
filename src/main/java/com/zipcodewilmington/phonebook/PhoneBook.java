8698546d939fee5b23e92fdc83e322a83d449bc0
ghost1497 <rfrankie04@gmail.com>
2018-02-13 19:43:07 -0500

    YASSSSSS

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -8,34 +8,53 @@ import java.util.TreeMap;
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
-    private TreeMap<String, String> phoneBook;
+    private TreeMap<String, ArrayList> phoneBook;
 
     public PhoneBook(){
-        this.phoneBook = new TreeMap<String, String>();
+        this.phoneBook = new TreeMap<String, ArrayList>();
     }
 
 
 
-    public void add(String name, String phoneNumber){
-        phoneBook.put(name, phoneNumber);
-        //System.out.println(phoneBook);
+    public void add(String name, String... phoneNumber){
+        ArrayList<String> phoneNumberList = new ArrayList<String>();
+        for (String number : phoneNumber){
+            phoneNumberList.add(number);
+        }
+        phoneBook.put(name, phoneNumberList);
     }
 
     public String lookUp(String name){
-        String result = phoneBook.get(name);
+        String result = String.valueOf(phoneBook.get(name));
+        return result.substring(1, result.length()-1);
+    }
+
+    public String reverseLookup(String number) {
+        String result = "";
+        for (Map.Entry<String, ArrayList> entry : phoneBook.entrySet()) {
+            if (entry.getValue().contains(number)) {
+                result = entry.getKey();
+            }
+        }
+;
         return result;
     }
 
-    public String getPhoneBook() {
+    public String getPhoneBook(){
         StringBuilder result = new StringBuilder();
-        for (Map.Entry<String, String> entry : phoneBook.entrySet()){
+        for (Map.Entry<String, ArrayList> entry : phoneBook.entrySet()){
             result.append(String.format("%-20s %8s\n", entry.getKey(), entry.getValue()));
         }
         return String.valueOf(result);
     }
 
-    public void removeEntry(String name){
+    public void removeRecord(String name){
         phoneBook.remove(name);
-        //System.out.println(getPhoneBook());
     }
+
+    public void removeEntry(String name, String number){
+        phoneBook.get(name).remove(number);
+    }
+
+
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,6 +1,7 @@
 package com.zipcodewilmington.phonebook;
 
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,14 +13,15 @@ public class PhoneBookTest {
     String name = "Steve Harvey";
     String phoneNumber = "2153755497";
     String name2 = "Frankie";
-    String pN2 = "2158883323";
+    String pN2 = "2158663323";
     String name3 = "Zoe";
     String pN3 = "7896785647";
+    String pN4 = "6574383954";
 
 
     @Before
     public void addEntryTest1(){
-        phoneBook.add(name, phoneNumber);
+        phoneBook.add(name, phoneNumber, pN4);
         phoneBook.add(name2, pN2);
         phoneBook.add(name3, pN3);
     }
@@ -29,24 +31,52 @@ public class PhoneBookTest {
         name = "Durp";
         phoneNumber = "2153755497";
         name2 = "Frankie";
-        pN2 = "2158883323";
+        pN2 = "2675677701";
         name3 = "Lenny Kravitz";
         pN3 = "3645274567";
-        phoneBook.add(name, phoneNumber);
+        phoneBook.add(name, phoneNumber, pN2);
         phoneBook.add(name2, pN2);
         phoneBook.add(name3, pN3);
     }
 
     @Test
     public void lookUpTest1(){
-        phoneBook.lookUp(name2);
-        //System.out.println(phoneBook.lookUp(name2));
+        String expected = "2158663323";
+        String actual = phoneBook.lookUp(name2);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void lookUpTest2(){
+        String expected = "2153755497, 6574383954";
+        String actual = phoneBook.lookUp(name);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void reverseLookupTest1(){
+        String expected = "Frankie";
+        String actual = phoneBook.reverseLookup(pN2);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeRecordTest1(){
+        phoneBook.removeRecord(name2);
+        String actual = phoneBook.getPhoneBook();
+        String expected = "Steve Harvey         [2153755497, 6574383954]\n" +
+                "Zoe                  [7896785647]\n";
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void removeEntryTest1(){
-        phoneBook.removeEntry(name2);
-        //System.out.println(phoneBook.getPhoneBook());
+        phoneBook.removeEntry(name, pN4);
+        String actual = phoneBook.getPhoneBook();
+        String expected = "Frankie              [2158663323]\n" +
+                "Steve Harvey         [2153755497]\n" +
+                "Zoe                  [7896785647]\n";
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
