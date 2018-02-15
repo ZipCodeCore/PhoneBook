faab5e5a981a82402e1a3207983d14b048791d24
Jessica Campbell <jessicacampbell@zipcoders-MacBook-Pro-42.local>
2018-02-15 15:16:39 -0500

    just wait on it

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,34 +1,101 @@
 package com.zipcodewilmington.phonebook;
 
-import apple.laf.JRSUIUtils;
-import com.sun.tools.corba.se.idl.StringGen;
-
+import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
 
-    private TreeMap<String, String> tmap;
+    private TreeMap<String String> contacts;
 
-   PhoneBook(){
+    public PhoneBook() {
 
-      this.tmap =new TreeMap<String, String>();
-
-   }
-
-   public void addAContact(String name, String number) {
-       this.tmap.put(name, number);
-   }
-
-   public void removeAContact(String name, String number){
-       this.tmap.remove(name, number);
-   }
-    public void lookupContact (String name, String number){
-
+        contacts = new TreeMap<>();
+    }
+    public void add (String name, String number){
+        contacts.put(name, number);
+    }
+    public String lookup (String name){
+        return contacts.get(name);
+    }
+    public void remove (String name){
+        contacts.remove(name);
+    }
+    public String display (){
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String, String>entry : contacts.entrySet()){
+            String name = entry.getKey();
+            String number = entry.getValue();
+            sb.append(name + "     " + number + "\n");
+        }
+        return sb.toString();
+    }
+    public String reverseLookup (String number){
+        for (Map.Entry <String, String>entry: contacts.entrySet()){
+            if (entry.getValue().equals(number){
+            }
+            return null;
+        }
     }
 
 
-       }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void addAContact(String name, String number) {
+        tmap.put(name, number);
+    }
+
+    public String listOfAllNames(){
+        StringBuilder result = new StringBuilder();
+        for (Map.Entry<String, String> entry: tmap.entrySet()){
+            result.append(String.format("%-20s %8s\n", entry.getKey(), entry.getValue()));
+            //formats the way a string will print out on console^^^^^^
+        }
+        return String.valueOf(result);
+    }
+
+    public void removeAContact(String name) {
+        tmap.remove(name);
+    }
+
+    public String lookupContactName(String name) {
+        if (tmap.containsKey(name)) {
+            return tmap.get(name);
+        } else {
+            return "This Name Does Not Exist";
+        }
+    }
+
+
+
+    public reverseLookUpNumber (String number){
+
+        }
+
+
+}
+
+
 
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,21 +1,77 @@
 package com.zipcodewilmington.phonebook;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.TreeMap;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+    // constructor
+    String name = "Jess";
+    String number = "1234567";
+    String nameA = "Alice";
+    String numberA = "1111111";
+    String nameB = "Sean";
+    String numberB = "2222222";
 
+    private PhoneBook phoneBook = new PhoneBook();
+    // instance
+
+
+   @Before
+   public void setUp(){
+       phoneBook = new PhoneBook();
+   }
+
+   @Test
+   public boolean testAddAContact() {
+    //instance of new phone book
+//    phoneBook.addAContact(name, number);
+//    Assert.assertTrue(phoneBook.addAContact(name, number).equals(name, number));
+       TreeMap.containsKey(String);
+       Assert.assertTrue(phoneBook.addAContact(name));
+
+   }
+@Test
+    public void removeAContact(String name){
+       //Given
+        String expected = "Null";
+
+        //When
+        phoneBook.addAContact(expected);
+        tmap.add(name);
+        //first must add a name to map
+
+        phoneBook.lookupContactName(expected);
+        String actual = phoneBook.removeAContact();
+        tmap.remove(name);
+        //Then
+        Assert.assertTrue(expected, actual);
+    }
     @Test
-    public void firstContactName() {
+    public void testLookupContactName (){
+       //Given
+       phoneBook.lookupContactName("Jess");
+       String expected = "1234567";
 
+       //When
+       String actual = phoneBook.lookupContactName("Jess");
+
+       //Then
+       Assert.assertEquals(expected, actual);
     }
 
+    @Test
+    public void printOut(){
+        System.out.println(phoneBook.listOfAllNames());
 
-
-
-
-
-
+    }
 }
+
+
+
+
