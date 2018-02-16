2c4feb5d9fa88e14c071e52935eff891fa486fbd
Jordan Elderidge <jordanelderidge@zipcoders-MacBook-Pro.local>
2018-02-16 16:00:29 -0500

    Complete

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,133 @@
 package com.zipcodewilmington.phonebook;
+import java.util.*;
+import java.util.Arrays;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+
+    private Map<String, List<String>> phoneBookRecords;
+
+    public PhoneBook() {
+        phoneBookRecords = new TreeMap<String, List<String>>();
+    }
+
+    /**
+     * adds a name and corresponding phone number to phone book
+     *
+     * @param name
+     * @param number
+     */
+    public Boolean nameAndNumber(String name, String number) {
+        if (phoneBookRecords.containsKey(name)) {
+            phoneBookRecords.get(name).add(number);
+            return true;
+        }
+        List<String> newNumber = new ArrayList<String>();
+        newNumber.add(number);
+        phoneBookRecords.put(name, newNumber);
+        return true;
+    }
+
+    /**
+     * uses name to retrieve and delete both name and phone number
+     *
+     * @param name
+     */
+    public boolean deleteNameAndNumber(String name) {
+        return phoneBookRecords.remove(name) != null;
+    }
+
+
+    /**
+     * retrieves phone number by name
+     *
+     * @param name
+     */
+    public String returnNumber(String name) {
+        return join(phoneBookRecords.get(name));
+    }
+
+    /**
+     * retrieves name by phone number
+     *
+     * @param number
+     */
+    public String returnName(String number) {
+        for (String nameKey : phoneBookRecords.keySet()) {
+            List<String> name4Number = phoneBookRecords.get(nameKey);
+            if(name4Number.contains(number)){
+                return nameKey;
+            }
+        }
+        return "";
+    }
+
+    /**
+     * returns alphabetical list of all names in phoneBook
+     */
+    public String listAllNames() {
+        StringBuilder list = new StringBuilder();
+        Formatter listNames = new Formatter(list);
+        for (String nameKey : phoneBookRecords.keySet()) {
+            listNames.format("Name: %s%n", nameKey);
+        }
+        return list.toString();
+    }
+
+    /**
+     * returns alphabetical list of all records in phoneBook
+     */
+    public String listAllNamesAndNumbers() {
+        StringBuilder list = new StringBuilder();
+        Formatter listNameNNumbers = new Formatter(list);
+        for (String nameKey : phoneBookRecords.keySet()) {
+            listNameNNumbers.format("Name: %s PhoneNumber: %-10s%n", nameKey, join(phoneBookRecords.get(nameKey)));
+        }
+        return list.toString();
+    }
+
+    /**
+     * joins a list by a delimiter
+     * @param listNum
+     * @return String
+     */
+    public static String join(List<String> listNum){
+
+        if(listNum.size() == 1){
+            return listNum.get(0);
+        }
+        StringBuilder sb = new StringBuilder();
+        for ( int i = 0; i < listNum.size(); i++)
+        {
+            sb.append(listNum.get(i));
+            if (i < listNum.size()-1){
+                sb.append(", ");
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+
+        PhoneBook lancasterPeople = new PhoneBook();
+        lancasterPeople.nameAndNumber("Javez", "3024785643");
+        lancasterPeople.nameAndNumber("Nick", "3029876543");
+        lancasterPeople.nameAndNumber("Jake", "3024556789");
+        lancasterPeople.nameAndNumber("Jayden", "3028990078");
+        lancasterPeople.nameAndNumber("Alex", "3025557896");
+        lancasterPeople.nameAndNumber("Logan", "3023324566");
+        lancasterPeople.nameAndNumber("Lance", "3021245432");
+
+
+        String x = lancasterPeople.listAllNamesAndNumbers();
+        System.out.println(x);
+
+        String y = lancasterPeople.listAllNames();
+        System.out.println(y);
+
+
+    }
+
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,114 @@
 package com.zipcodewilmington.phonebook;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+
+    private static PhoneBook PhoneBookTest;
+
+    public void setUp() {
+        PhoneBookTest = new PhoneBook();
+    }
+
+    @Test
+    public void recordShouldAddToPhoneBook() {
+        PhoneBook phoneBookContact = new PhoneBook();
+        phoneBookContact.nameAndNumber ("Nick", "3029876543");
+        String expectedValue = phoneBookContact.returnNumber("Nick");
+        Assert.assertEquals(expectedValue, "3029876543");
+    }
+
+    @Test
+    public void recordShouldAddMultipleNumbersToPerson() {
+        PhoneBook phoneBookContact = new PhoneBook();
+        phoneBookContact.nameAndNumber ("Nick", "3029876543");
+        phoneBookContact.nameAndNumber ("Nick", "3029876541");
+        phoneBookContact.nameAndNumber ("Nick", "3029876546");
+        String actualValue = phoneBookContact.returnNumber("Nick");
+        Assert.assertEquals( "3029876543, 3029876541, 3029876546", actualValue);
+    }
+
+    @Test
+    public void ShouldRetrieveNumbersByName() {
+        PhoneBook phoneBookContact= new PhoneBook();
+        phoneBookContact.nameAndNumber ("Nick", "3029876543");
+        String expectedValue = phoneBookContact.returnNumber("Nick");
+        Assert.assertEquals(expectedValue, "3029876543");
+    }
+
+    @Test
+    public void ShouldRetrieveMultipleNumbersByName() {
+        PhoneBook phoneBookContact = new PhoneBook();
+        phoneBookContact.nameAndNumber("Jake", "3024556789");
+        phoneBookContact.nameAndNumber("Jake", "3024556788");
+        phoneBookContact.nameAndNumber("Jake", "3024556787");
+        String actualValue = phoneBookContact.returnNumber("Jake");
+        Assert.assertEquals( "3024556789, 3024556788, 3024556787", actualValue);
+    }
+
+
+    @Test
+    public void receiveNameUsingNumber() {
+        PhoneBook phoneBookContact = new PhoneBook();
+        phoneBookContact.nameAndNumber ("Nick", "3029876543");
+        String expectedValue = phoneBookContact.returnName("3029876543");
+        Assert.assertEquals(expectedValue, "Nick");
+    }
+
+
+    @Test
+    public void recordShouldDeleteFromPhoneBook() {
+        PhoneBook phoneBookContact = new PhoneBook();
+        phoneBookContact.nameAndNumber ("Nick", "3029876543");
+        phoneBookContact.deleteNameAndNumber("Nick");
+        String expectedValue = phoneBookContact.listAllNames();
+        Assert.assertEquals(expectedValue, "");
+    }
+
+    @Test
+    public void DeletePersonFromPhoneBookWithMoreThanOneNumber() {
+        PhoneBook phoneBookContact = new PhoneBook();
+        phoneBookContact.nameAndNumber ("Javez", "3024785643");
+        phoneBookContact.nameAndNumber ("Javez", "3024785642");
+        phoneBookContact.nameAndNumber ("Javez", "3024785656");
+        phoneBookContact.deleteNameAndNumber("Javez");
+        String expectedValue = phoneBookContact.listAllNames();
+        Assert.assertEquals(expectedValue, "");
+    }
+
+
+    @Test
+    public void listAllNamesAlphabeticalOrder() {
+        PhoneBook phoneBookContact= new PhoneBook();
+        phoneBookContact.nameAndNumber("Alex", "3025557896");
+        phoneBookContact.nameAndNumber("Jake", "3024556789");
+        phoneBookContact.nameAndNumber ("Javez", "3024785643");
+        phoneBookContact.nameAndNumber ("Jordan", "3028990078");
+        phoneBookContact.nameAndNumber("Lance", "3021245432");
+        String actual = phoneBookContact.listAllNames();
+        String testList = "Name: Alex\nName: Jake\nName: Javez\nName: Jordan\nName: Lance\n";
+        Assert.assertEquals(testList, actual);
+    }
+
+
+    @Test
+    public void listAllNamesAndNumbers() {
+
+        PhoneBook phoneBookContact = new PhoneBook();
+        phoneBookContact.nameAndNumber ("Javez", "3024785643");
+        phoneBookContact.nameAndNumber("Alex", "3025557896");
+        phoneBookContact.nameAndNumber("Jake", "3024556789");
+        phoneBookContact.nameAndNumber ("Nick", "3029876543");
+        phoneBookContact.nameAndNumber("Lance", "3021245432");
+        String actual = phoneBookContact.listAllNames();
+        String testList = "Name: Alex\nName: Jake\nName: Javez\nName: Lance\nName: Nick\n";
+        Assert.assertEquals(testList, actual);
+    }
+
+
 }
+
+
