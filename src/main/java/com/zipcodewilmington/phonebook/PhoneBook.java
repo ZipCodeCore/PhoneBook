5defe20bf4360c632a7269e4260132fa2a4eac67
Thao Nguyen <thaonguyen@zipoders-mbp.lan>
2019-02-19 20:41:04 -0500

    done

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,62 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    //Field
+    //accessor(private) type (Map<String, String>)
+    private Map<String, String> entries;
+
+    //Constructor
+    public PhoneBook() {
+        //intializing the entries
+        entries = new TreeMap<String, String>();
+    }
+
+    public void add(String name, String number) {
+        entries.put(name, number);
+    }
+
+    public Integer size() {
+        return entries.size();
+    }
+
+    public void remove(String name) {
+        entries.remove(name);
+    }
+
+    public String lookUp(String name) {
+        return entries.get(name);
+    }
+
+    public String reverseLookUp(String phoneNumber) {
+        for (Map.Entry<String, String> entry : entries.entrySet()) {
+            if (entry.getValue().equals(phoneNumber)) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+
+    public String display() {
+        String result = "";
+        for (Map.Entry<String, String> entry : entries.entrySet()) {
+            result += entry.getKey() + " " + entry.getValue() + "\n";
+
+        }
+
+//        Set<Map.Entry<String, String>> entriesSet = entries.entrySet();
+//
+//        for (int i = 0; i < entriesSet.size(); i++) {
+//            Map.Entry<String, String> entry = entriesSet.get(i);
+//        }
+        return result;
+    }
 }
+
+
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,91 @@
 package com.zipcodewilmington.phonebook;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+
+    @Test
+    public void testAdd(){
+        //Given - create a new instance of the class PhoneBook
+        PhoneBook phoneBook = new PhoneBook();
+        Integer expected = 1;
+
+        //When
+        phoneBook.add("Mocha","123");
+
+        //Then
+        Integer actual = phoneBook.size();
+        Assert.assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void testRemove(){
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Mocha","215-555-7890");
+        Integer expected = 0;
+
+        //When
+        phoneBook.remove("Mocha");
+
+        //Then
+        Integer actual = phoneBook.size();
+        Assert.assertEquals(expected,actual);
+    }
+
+
+    @Test
+    public void testLoopUp(){
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+        String expected = "215-555-7890";
+        String name = "Mocha";
+        phoneBook.add(name, expected);
+
+        //When
+        //when the method returns, store the result into a variable (actual)
+        //use the actual to test that the actual is equal to expected
+        String actual = phoneBook.lookUp(name);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testReverseLoopUp() {
+        //Given
+        PhoneBook phoneBook = new PhoneBook();
+        String expected = "Mocha";
+        String phoneNumber = "215-555-7890";
+        phoneBook.add(expected,phoneNumber);
+
+        //When
+        String actual = phoneBook.reverseLookUp(phoneNumber);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void testDisplay(){
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Zebra", "111-222-333");
+        phoneBook.add("Dog", "222-444-4444");
+
+        //When
+        String actual = phoneBook.display();
+        String expected = "Dog 222-444-4444\n" +
+                          "Zebra 111-222-333\n";
+        //Then
+        Assert.assertEquals(expected,actual);
+
+
+
+    }
+
+
 }
