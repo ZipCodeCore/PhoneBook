f8afc7d4edb50f592167dd3211ae36fff71e4ce5
John Tokhi <56596400+tokhij@users.noreply.github.com>
2019-11-04 22:40:18 -0500

    started

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,13 +1,15 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    Map<String, List<String>> theMap;
+
     public PhoneBook(Map<String, List<String>> map) {
+        this.theMap = map;
     }
 
     public PhoneBook() {
@@ -15,16 +17,24 @@ public class PhoneBook {
     }
 
     public void add(String name, String phoneNumber) {
+        theMap.put(name, Collections.singletonList(phoneNumber));
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        theMap.put(name, Arrays.asList(phoneNumbers));
     }
 
     public void remove(String name) {
+        if (hasEntry(name)) {
+            theMap.remove(name);
+        }
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        if(theMap.containsKey(name)){
+            return true;
+        }
+        return false;
     }
 
     public List<String> lookup(String name) {
