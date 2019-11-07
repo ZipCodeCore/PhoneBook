1f5d279c2b72b5784dfae21fba3203d8f537f64b
MikeGray31 <michaelkgray31@gmail.com>
2019-11-07 17:49:50 -0500

    Finished

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,45 +1,68 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+
+    Map <String, List<String>> map;
+    private static final java.util.logging.Logger logger = Logger.getGlobal();
+
+
     public PhoneBook(Map<String, List<String>> map) {
+        this.map = map;
     }
 
     public PhoneBook() {
-        this(null);
+        this.map = new LinkedHashMap<String, List<String>>();
     }
 
     public void add(String name, String phoneNumber) {
+        map.put(name, new ArrayList<>());
+        map.get(name).add(phoneNumber);
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        map.put(name, new ArrayList<>());
+        map.get(name).addAll(Arrays.asList(phoneNumbers));
     }
 
     public void remove(String name) {
+        map.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        return map.containsKey(name);
     }
 
     public List<String> lookup(String name) {
-        return null;
+        return map.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
-        return null;
+
+            for (Map.Entry<String, List<String >> entry : map.entrySet()) {
+                if (entry.getValue().contains(phoneNumber)){
+                    return entry.getKey();
+                }
+            }
+            return null;
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        List<String> result = new ArrayList<>();
+
+        for (String entry : map.keySet()) {
+            result.add(entry);
+        }
+        return result;
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return this.map;
     }
 }
--- src/test/java/com/zipcodewilmington/phonebook/RemoveTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/RemoveTest.java
@@ -11,7 +11,7 @@ public class RemoveTest {
         String name = "John";
         String phoneNumber = "302-555-4545";
         phoneBook.add(name, phoneNumber);
-        Assert.assertTrue(phoneBook.hasEntry(phoneNumber));
+        Assert.assertTrue(phoneBook.hasEntry(name));
 
         // when
         phoneBook.remove(name);
@@ -27,7 +27,7 @@ public class RemoveTest {
         String name = "Joe";
         String phoneNumber = "302-554-4545";
         phoneBook.add(name, phoneNumber);
-        Assert.assertTrue(phoneBook.hasEntry(phoneNumber));
+        Assert.assertTrue(phoneBook.hasEntry(name));
 
         // when
         phoneBook.remove(name);
@@ -44,7 +44,7 @@ public class RemoveTest {
         String name = "Smith";
         String phoneNumber = "302-554-4535";
         phoneBook.add(name, phoneNumber);
-        Assert.assertTrue(phoneBook.hasEntry(phoneNumber));
+        Assert.assertTrue(phoneBook.hasEntry(name));
 
         // when
         phoneBook.remove(name);
