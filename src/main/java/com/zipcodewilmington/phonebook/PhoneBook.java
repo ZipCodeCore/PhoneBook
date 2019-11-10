fcbe03fb8a1acaa6b0d0c02eba7ff70ee4ff5087
Maira Botelho <mairabotelho@zipcoders-MacBook-Pro-3.local>
2019-11-09 22:58:25 -0500

    lab done

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,45 +1,75 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+
+    Map<String, List<String>> map;
+    List<String> list;
+
     public PhoneBook(Map<String, List<String>> map) {
+        this.map = map;
     }
 
     public PhoneBook() {
-        this(null);
+        map = new LinkedHashMap<>();
     }
 
     public void add(String name, String phoneNumber) {
+
+        list = new ArrayList<>();
+        list.add(phoneNumber);
+        map.put(name, list);
+
     }
 
     public void addAll(String name, String... phoneNumbers) {
+
+        list = new ArrayList<>();
+        list.addAll(Arrays.asList(phoneNumbers));
+        map.put(name, list);
     }
 
     public void remove(String name) {
+        map.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+
+        for (Map.Entry<String, List<String>> entry: map.entrySet())
+            if (entry.getValue().contains(name))
+                return true;
+
+        return false;
     }
 
     public List<String> lookup(String name) {
-        return null;
+
+        return map.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
+
+        for (Map.Entry<String, List<String>> entry: map.entrySet())
+            if (entry.getValue().contains(phoneNumber))
+                return entry.getKey();
+
         return null;
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        list = new ArrayList<>();
+
+        for (Map.Entry<String, List<String>> entry : map.entrySet())
+            list.add(entry.getKey());
+
+        return list;
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return map;
     }
 }
