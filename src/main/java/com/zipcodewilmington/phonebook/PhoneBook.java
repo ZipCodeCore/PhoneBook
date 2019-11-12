4468c57fb8aac38c7cf91e33cfee4f1f71c819cf
kshields412 <kshields412@gmail.com>
2019-11-11 19:44:33 -0500

    checkpoint

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,37 +1,48 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    private final Map<String, List<String>> map;
+
     public PhoneBook(Map<String, List<String>> map) {
+        this.map = map;
     }
 
     public PhoneBook() {
-        this(null);
+
+        this.map = new TreeMap<>();
     }
 
     public void add(String name, String phoneNumber) {
+        List<String> phoneNumberList = map.get(name);
+        phoneNumberList.add(phoneNumber);
+        map.put(phoneNumber, phoneNumberList);
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        for(String phoneNumber : phoneNumbers){
+            add(name, phoneNumber);
+        }
     }
 
     public void remove(String name) {
+        map.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        return map.get(name) != null;
     }
 
     public List<String> lookup(String name) {
-        return null;
+        return map.get(name);
     }
 
     public String reverseLookup(String phoneNumber)  {
+
         return null;
     }
 
